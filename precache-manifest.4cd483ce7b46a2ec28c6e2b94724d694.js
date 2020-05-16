af17fcb06f1fe9eae4921ffa6038ceef77ce4741
kalai-react <kvanan@gmails.com>
2020-05-16 23:25:57 +0530

    Updates

---
+++ precache-manifest.4cd483ce7b46a2ec28c6e2b94724d694.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "235f7f33d17ec1112b3679dfeacfa3e3",
+    "url": "/kalaitest/index.html"
+  },
+  {
+    "revision": "3eaffa90f57d23587484",
+    "url": "/kalaitest/static/css/main.093df822.chunk.css"
+  },
+  {
+    "revision": "bab2c7764258278f6024",
+    "url": "/kalaitest/static/js/2.2fba1178.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/kalaitest/static/js/2.2fba1178.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eaffa90f57d23587484",
+    "url": "/kalaitest/static/js/main.af38e5bd.chunk.js"
+  },
+  {
+    "revision": "b1a7fed55fb185e16453",
+    "url": "/kalaitest/static/js/runtime-main.4f86927a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kalaitest/static/media/logo.5d5d9eef.svg"
+  }
+]);
